b9cf660fb930d20b75d343d1142394f497c53bdf
Karim Mahfouz <karimmahfouznd@gmail.com>
2022-09-08 22:31:58 -0400

    Add border around level to contain ball Add clamping to ball angle

--- Ball.gd
+++ Ball.gd
@@ -1,8 +1,9 @@
 extends RigidBody2D
 
-export var shotStrength: float = 500
+export var shotStrength = 0
 var angle = -45
-
+var minAngle= 0
+var maxAngle= -90
 # Called when the node enters the scene tree for the first time.
 
 func _ready() -> void:
@@ -12,13 +13,16 @@ func _ready() -> void:
 # warning-ignore:unused_argument
 
 func _process(delta):
-	var direction = Vector2(1,0).rotated(deg2rad(angle))
-	if Input.is_action_just_pressed("Launch"):
+	var direction = Vector2(1,0).rotated(clamp(deg2rad(angle), deg2rad(maxAngle), deg2rad(minAngle)))
+	if Input.is_action_pressed("Launch"):
+		shotStrength += 50
+		print(shotStrength)
+	elif Input.is_action_just_released("Launch"):
 		var velocity = direction * shotStrength
 		apply_impulse(Vector2.ZERO, velocity)
-	elif Input.is_action_pressed("Angle down"):
+	elif Input.is_action_pressed("Angle down") && angle < 0:
 		angle += 1
 		print(angle)
-	elif Input.is_action_pressed("Angle up"):
+	elif Input.is_action_pressed("Angle up") && angle > -90:
 		angle -= 1
 		print(angle)
