733fc2c5b24ef6611fa659744a364cfe85a1872f
Karim Mahfouz <karimmahfouznd@gmail.com>
2022-09-13 12:46:51 -0400

    Add Kablam.wav to assets

---
+++ AudioStreamPlayer2D.gd
@@ -0,0 +1,18 @@
+extends AudioStreamPlayer2D
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+var counter = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_released("Launch") && counter > 4 :
+		play()
+		counter += 1
