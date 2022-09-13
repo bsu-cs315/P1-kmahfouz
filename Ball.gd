26577ab564d76d6a4193a192385e290f35e62add
Karim Mahfouz <karimmahfouznd@gmail.com>
2022-09-13 12:38:13 -0400

    Add ball limit Add Kablam.wav sound file

--- Ball.gd
+++ Ball.gd
@@ -3,6 +3,7 @@ export var shotStrength = 0
 var angle = -45
 var minAngle= 0
 var maxAngle= -90
+var counter = 0
 # Called when the node enters the scene tree for the first time.
 
 func _ready() -> void:
@@ -13,16 +14,19 @@ func _ready() -> void:
 
 func _process(delta):
 	var direction = Vector2(1,0).rotated(clamp(deg2rad(angle), deg2rad(maxAngle), deg2rad(minAngle)))
-	if Input.is_action_pressed("Launch"):
+	if counter > 4:
+		shotStrength = 0
+	elif Input.is_action_pressed("Launch"):
 		shotStrength += 50
 		print(shotStrength)
 	elif Input.is_action_just_released("Launch"):
 		var velocity = direction * shotStrength
 		apply_impulse(Vector2.ZERO, velocity)
+		counter += 1
 	elif Input.is_action_pressed("Angle down") && angle < 0:
 		angle += 1
 		print(angle)
 	elif Input.is_action_pressed("Angle up") && angle > -90:
 		angle -= 1
 		print(angle)
-
+		
